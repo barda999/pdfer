040b938f5d6029c3e16d752b99d43fa231a58450
barda999 <zamanian.bardia@gmail.com>
2023-06-02 11:27:45 +0000

    Initial Commit

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express')
+const PDFDocument = require('pdfkit')
+const { jsPDF }  =  require("jspdf");
+const Vazirmatn = require("./Vazirmatn-Regular-normal.js");
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const doc = new PDFDocument();
+
+  doc
+  .font('Vazirmatn-Regular.ttf')
+  .fontSize(25)
+  .text('سلام بر شما', 100, 100); 
+  
+  res.setHeader('Content-disposition', 'attachment; filename=something.pdf')
+  res.setHeader('Content-type', 'application/pdf')
+
+  doc.pipe(res)
+  doc.end()
+})
+
+app.get('/jspdf', (req, res) => {
+  const doc = new jsPDF();
+
+  doc.setFont('Vazirmatn-Regular')
+  doc.text("سلام بر تو", 10, 10);
+  
+  res.setHeader('Content-disposition', 'attachment; filename=something.pdf')
+  res.setHeader('Content-type', 'application/pdf')
+  res.send(doc.output());
 })
 
 app.listen(port, () => {
